85af423fe51ad80939db4385412f50f540ef8c60
Patrick Aoun <patrick.aoun@etu.enseeiht.fr>
2012-03-18 15:45:29 +0100

    changement de la hiérarchie

---
+++ app1/appli1.rb
@@ -0,0 +1,29 @@
+$: << File.dirname(__FILE__)
+
+require 'sinatra'
+set :port, 9191
+helpers do 
+  def current_user
+    session["current_user"]
+  end
+
+  def disconnect
+    session["current_user"] = nil
+  end
+end
+
+enable :sessions
+
+get '/' do
+	erb :"main_page"
+end
+
+
+
+get '/protected' do
+  if !session[:current_user_app].nil? || !params["login"].nil? 
+    erb :"protected"
+  else
+	redirect "http://localhost:4567/appli1/sessions/new?origin=/protected"
+  end
+end
